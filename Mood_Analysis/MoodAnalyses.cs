5f5896a725976c672af1b680e6edee35782b55f6
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-11 01:48:44 +0530

    Mood added SAD or HAPPY

---
+++ Mood_Analysis/MoodAnalyses.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mood_Analysis
+{
+    public static class MoodAnalyses
+    {
+        static void Main(string[] args)
+        {
+            String input = Console.ReadLine();
+            Mood mood = new Mood();
+            Console.WriteLine(mood.analyserMood(input));
+        }
+    }
+    public class Mood
+    {
+        public String analyserMood(String message)
+        {
+                if (message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }   
+        }
+    }
+}
