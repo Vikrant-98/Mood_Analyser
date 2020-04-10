22c180c490eac23fe5d8f1c11aa5682857922421
Vikrant-98 <vikrantchitte5398@gmail.com>
2020-04-11 01:56:36 +0530

    Test case SAD

--- MoodAnalysesTests/MoodAnalyserTest.cs
+++ MoodAnalysesTests/MoodAnalyserTest.cs
@@ -6,5 +6,13 @@ namespace MoodAnalysesTests
 {
     public class MoodAnalyserTest
     {
+        [Fact]
+        public void MoodAnalyserSadTest()
+        {
+            Mood mood = new Mood();
+            String actual = mood.analyserMood("I am in Sad Mood");
+            Assert.Equal("SAD", actual);
+        }
+
     }
 }
